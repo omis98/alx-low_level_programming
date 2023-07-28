dcf8179ecf5ca18ee69ea121fe15fb789982fc7c
omis98 <elassasmohamed8@outlook.fr>
2023-07-27 18:14:15 -0700

    0x12. C - Singly linked lists

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,15 @@
+/*free linked list,head list*/
+#include <stdlib.h>
+#include "lists.h"
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	while (head)
+	{
+		temp = head->next;
+		free(head->str);
+		free(head);
+		head = temp;
+	}
+}
