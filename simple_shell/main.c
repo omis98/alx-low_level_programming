e7e156e45cc7fc34e90361ec8b77561751f15a50
omis98 <elassasmohamed8@outlook.fr>
2023-08-23 15:22:21 -0700

    simple shell

--- simple_shell/main.c
+++ simple_shell/main.c
@@ -1,53 +1,13 @@
-#include "main.h"
+#include <stdio.h>
+#include "simple_shell.h"
+
 /**
- * sigint_handler - sginal
- * @signum
+ * main - Start point of program.
  *
- * Return : void
+ * Return: 0 if success, 1 if failed.
  */
-void sigint_handler(int signum) 
-{
-        (void)signum;
-	
-	if (isatty(STDIN_FILENO))
-        	write(STDOUT_FILENO, "\n&& ", 4);
-        exit(0);
-}
-/**
- * main - entry point
- * Return: int
-*/
 int main(void)
 {
-        int i = 1, nread = 0;
-        size_t len = 0;
-        char *buffer = NULL;
-        int fail;
-
-        signal(SIGINT, sigint_handler);
-
-        while (i == 1)
-        {
-                if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "&& ", 3);
-                fflush(stdout);
-                nread = getline(&buffer, &len, stdin);
-                if (nread != -1)
-                {
-                        fail = get_command(buffer);
-                        if (fail == 100)
-                        {
-				free(buffer);
-				exit(0);
-			}
-                        fflush(stdout);
-                }
-                else
-                {
-                        i = 0;
-			if (buffer != NULL)
-				free(buffer);
-                }
-        }
-        return (0);
+	simple_shell();
+	return (0);
 }
